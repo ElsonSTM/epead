363bd25433f80f75950bbda61608d63a18fbbac1
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-04-12 23:52:04 -0300

    Project MS AuthUser Start / Configure PostgreSql

---
+++ authuser/src/main/java/com/epead/authu/AuthuApplication.java
@@ -0,0 +1,13 @@
+package com.epead.authu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthuApplication.class, args);
+    }
+
+}
---
+++ authuser/src/test/java/com/epead/authu/AuthuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epead.authu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
