3d9ad97ec938dfccf5165761f48c4d1592dc69ec
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-04-14 08:55:39 -0300

    Implementation JsonView in resources calls.

--- authuser/src/main/java/com/epead/authu/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/epead/authu/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.epead.authu.enums.UserStatus;
 import com.epead.authu.enums.UserType;
 import com.epead.authu.models.UserModel;
 import com.epead.authu.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,11 +24,13 @@ public class AuthenticationController {
     UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto){
-        if(userService.existsByUsername(userDto.getUsername())){
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                               @JsonView(UserDto.UserView.RegistrationPost.class)
+                                                       UserDto userDto) {
+        if (userService.existsByUsername(userDto.getUsername())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
-        if(userService.existsByEmail(userDto.getEmail())){
+        if (userService.existsByEmail(userDto.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!");
         }
 
