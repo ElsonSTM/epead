fc3613748218cfc3a4ea7705621e0bbf00b76b62
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-04-23 12:17:02 -0300

    Implement CRUD - MS Lesson (LessonController)

--- course/src/main/java/com/epead/course/services/LessonService.java
+++ course/src/main/java/com/epead/course/services/LessonService.java
@@ -1,4 +1,17 @@
 package com.epead.course.services;
 
+import com.epead.course.models.LessonModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface LessonService {
+    LessonModel save(LessonModel lessonModel);
+
+    Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId);
+
+    void delete(LessonModel lessonModel);
+
+    List<LessonModel> findAllByModule(UUID moduleId);
 }
