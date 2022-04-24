6bb23b03ab6f435bb4d8febcbf5a6120005afda3
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-04-23 21:30:21 -0300

    Spedification filte advancer method (Lesson and Modules)

--- course/src/main/java/com/epead/course/controllers/ModuleController.java
+++ course/src/main/java/com/epead/course/controllers/ModuleController.java
@@ -5,8 +5,13 @@ import com.epead.course.models.CourseModel;
 import com.epead.course.models.ModuleModel;
 import com.epead.course.services.CourseService;
 import com.epead.course.services.ModuleService;
+import com.epead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -72,8 +77,11 @@ public class ModuleController {
     }
 
     @GetMapping("/courses/{courseId}/modules")
-    public ResponseEntity<List<ModuleModel>> getAllModules(@PathVariable(value="courseId") UUID courseId){
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(courseId));
+    public ResponseEntity<Page<ModuleModel>> getAllModules(@PathVariable(value="courseId") UUID courseId,
+                                                           SpecificationTemplate.ModuleSpec spec,
+                                                           @PageableDefault(page = 0, size = 10, sort = "moduleId",
+                                                           direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
     }
 
     @GetMapping("/courses/{courseId}/modules/{moduleId}")
