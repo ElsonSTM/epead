cce0c623adb5b3b90d5af85b99e441a097a7f9f4
Elson Pinheiro <elson.silva@ufopa.edu.br>
2022-04-26 22:36:07 -0300

    Composition Pattern API - Prepare GetAll Methods

--- authuser/src/main/java/com/epead/authu/specifications/SpecificationTemplate.java
+++ authuser/src/main/java/com/epead/authu/specifications/SpecificationTemplate.java
@@ -1,5 +1,6 @@
 package com.epead.authu.specifications;
 
+import com.epead.authu.models.UserCourseModel;
 import com.epead.authu.models.UserModel;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
@@ -7,13 +8,25 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
+import javax.persistence.criteria.Join;
+import java.util.UUID;
+
 public class SpecificationTemplate {
 
     @And({ //notação para combinar filtros abaixo
         @Spec(path = "userType", spec = Equal.class),
         @Spec(path = "userStatus", spec = Equal.class),
         @Spec(path = "email", spec = Like.class),
+        @Spec(path = "fullName", spec = Like.class)
     })
-    public interface UseSpec extends Specification<UserModel> {}
+    public interface UserSpec extends Specification<UserModel> {}
+
+    public static Specification<UserModel> userCourseId(final UUID courseId) {
+        return (root, query, cb) -> {
+          query.distinct(true);
+            Join<UserModel, UserCourseModel> userProd = root.join("usersCourses");
+          return cb.equal(userProd.get("courseId"), courseId);
+        };
+    }
 }
 
