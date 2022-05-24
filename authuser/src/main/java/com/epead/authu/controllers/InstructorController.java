f1297b7c9f73215fdea52dd20f5d168cf511d382
ElsonSTM <86759660+ElsonSTM@users.noreply.github.com>
2022-05-23 21:46:15 -0300

    Tipo User Instructor para Criar Courses

---
+++ authuser/src/main/java/com/epead/authu/controllers/InstructorController.java
@@ -0,0 +1,43 @@
+package com.epead.authu.controllers;
+
+import com.epead.authu.dtos.InstructorDto;
+import com.epead.authu.enums.UserType;
+import com.epead.authu.models.UserModel;
+import com.epead.authu.services.UserService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600) //permitir o acesso de tds as origens
+@RequestMapping("/instructors")
+public class InstructorController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/subscription")
+    public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto){
+        Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
+        if(!userModelOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+}
---
+++ authuser/src/main/java/com/epead/authu/dtos/InstructorDto.java
@@ -0,0 +1,13 @@
+package com.epead.authu.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+public class InstructorDto {
+
+    @NotNull
+    private UUID userId;
+}
