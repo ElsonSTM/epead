a37f41d91fd00ef548607278049f3db898aabe22
ElsonSTM <86759660+ElsonSTM@users.noreply.github.com>
2022-05-27 00:15:38 -0300

    Eureka Clients nos Microservices de Negócio

--- authuser/src/main/java/com/epead/authu/AuthuApplication.java
+++ authuser/src/main/java/com/epead/authu/AuthuApplication.java
@@ -2,8 +2,10 @@ package com.epead.authu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthuApplication {
 
     public static void main(String[] args) {
